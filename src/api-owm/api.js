7a3af8d7b91f920f7fa44ca47f5c3f30dbb02fd1
muhammad-z <mhd.z95@hotmail.com>
2023-01-22 20:49:22 +0300

    securing api keys

--- src/api-owm/api.js
+++ src/api-owm/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
- baseURL: 'https://api.openweathermap.org',
+  baseURL: 'https://api.openweathermap.org',
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,8 +9,12 @@ const instance = axios.create({
 
 instance.interceptors.request.use(
   (config) => {
-    config.params = {...config.params, appid: "aec112ac0df781f177b949f3e8e1b7b4"};
- 
+    config.params = {
+      ...config.params, appid: (
+        typeof process !== "undefined" ? process.env.VITE_API_KEY
+          : import.meta.env.VITE_API_KEY)
+    };
+
     return config;
   },
   (error) => {
