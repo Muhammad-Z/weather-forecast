55376cfe6cc45f48c1c41b42cb016188c8c7d91e
muhammad-z <mhd.z95@hotmail.com>
2023-01-22 15:36:26 +0300

    Day & night animation

--- src/CardsContainer.jsx
+++ src/CardsContainer.jsx
@@ -8,7 +8,7 @@ export default function CardsContainer() {
   return (<div className="cards-container">
     {Array.isArray(forecast?.fiveDays) ? forecast.fiveDays.map((elem, index) =>
       <DayCard key={elem.date} day={elem.epoch}
-        icon={elem.weather.d.icon} temp={elem.temp_avg} index={index} />)  : null }
+        icon={{d: elem.weather.d.icon, n:elem.weather.n.icon}} temp={elem.temp_avg} index={index} />)  : null }
   </div>
   )
 }
--- src/DayCard.jsx
+++ src/DayCard.jsx
@@ -1,17 +1,30 @@
+import { useEffect, useState } from 'react';
 import { useForecastDispatch } from './ForecastContext.jsx';
 
 export default function DayCard({ day, icon, temp, index }) {
   const dispatch = useForecastDispatch();
+  const [animate, setAnimate] = useState('');
+  const [showup, setShowup] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      if (animate) { setShowup(false); setAnimate(''); }
+      else { setShowup(true); setAnimate('animate'); }
+    }, 5000)
+  }, [animate])
 
   function handleClick() {
-    dispatch({type: 'selectDay', payload: index});
+    dispatch({ type: 'selectDay', payload: index });
   }
 
-  return (
+  return (<>
     <div className="day-card box" onClick={handleClick} >
       <span>{day}</span>
-      <img src={`https://openweathermap.org/img/wn/${icon}.png`} />
+      <img className={`${showup ? 'animate' : 'hidden'}`}
+        src={`https://openweathermap.org/img/wn/${icon.d}.png`} />
+      <img className={`${showup ? 'hidden' : 'animate'}`}
+        src={`https://openweathermap.org/img/wn/${icon.n}.png`} />
       <span>{temp}</span>
     </div>
+  </>
   )
 }
